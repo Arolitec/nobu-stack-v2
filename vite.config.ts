d608b84c8186c74fd71d1c61d242e8e1884477ed
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-10-06 13:18:24 +0000

    refactor(vitest): extract vitest config to its own file
    #13

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { vitePlugin as remix } from '@remix-run/dev'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import { envOnlyMacros } from 'vite-env-only'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { coverageConfigDefaults, defaultExclude } from 'vitest/config'
 import remixConfig from './remix.config'
 
 export default defineConfig({
@@ -12,27 +11,5 @@ export default defineConfig({
 	},
 	build: {
 		target: 'esnext',
-	},
-	test: {
-		globals: true,
-		environment: 'happy-dom',
-		setupFiles: ['./test/setup.ts'],
-		globalSetup: ['./test/global-setup.ts'],
-		include: ['app/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
-		exclude: [...defaultExclude, 'tests/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
-		poolMatchGlobs: [
-			['app/**/(loader|action).{test,spec}.?(c|m)[jt]s?(x)', 'forks'],
-		],
-		env: loadEnv('', process.cwd(), ''),
-		coverage: {
-			provider: 'v8',
-			include: ['app/**'],
-			clean: true,
-			exclude: [
-				...coverageConfigDefaults.exclude,
-				'app/entry.*.tsx',
-				'app/root.tsx',
-			],
-		},
 	},
 })
