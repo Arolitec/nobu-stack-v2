aeaf4d111712be34924f5b216aab0644337e7c8e
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-10-06 13:01:15 +0000

    chore(vitest): setup vitest and its deps
    - add vitest, vitest ui and v8-coverage
    - update vite config to add test config
    - add happy-dom and setup dom matcher
    
    #13

---
+++ test/global-setup.ts
@@ -0,0 +1,7 @@
+export async function setup() {
+	// Global setup
+}
+
+export async function teardown() {
+	// Global teardown
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,38 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import { envOnlyMacros } from 'vite-env-only';
-import tsconfigPaths from "vite-tsconfig-paths";
-import remixConfig from './remix.config';
+import { vitePlugin as remix } from '@remix-run/dev'
+import { defineConfig, loadEnv } from 'vite'
+import { envOnlyMacros } from 'vite-env-only'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { coverageConfigDefaults, defaultExclude } from 'vitest/config'
+import remixConfig from './remix.config'
 
 export default defineConfig({
-  plugins: [
-    remix(remixConfig),
-    tsconfigPaths(),
-    envOnlyMacros()
-  ],
-  server: {
-    host: process.env.NODE_ENV === 'production' ? undefined : true // Allow access on local network
-  },
-  build: {
-    target: 'esnext'
-  }
-});
+	plugins: [remix(remixConfig), tsconfigPaths(), envOnlyMacros()],
+	server: {
+		host: process.env.NODE_ENV === 'production' ? undefined : true, // Allow access on local network
+	},
+	build: {
+		target: 'esnext',
+	},
+	test: {
+		globals: true,
+		environment: 'happy-dom',
+		setupFiles: ['./test/setup.ts'],
+		globalSetup: ['./test/global-setup.ts'],
+		include: ['app/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+		exclude: [...defaultExclude, 'tests/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+		poolMatchGlobs: [
+			['app/**/(loader|action).{test,spec}.?(c|m)[jt]s?(x)', 'forks'],
+		],
+		env: loadEnv('', process.cwd(), ''),
+		coverage: {
+			provider: 'v8',
+			include: ['app/**'],
+			clean: true,
+			exclude: [
+				...coverageConfigDefaults.exclude,
+				'app/entry.*.tsx',
+				'app/root.tsx',
+			],
+		},
+	},
+})
