3bd717b465996c7617de3e49a55ba1812e0a223d
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-10-07 11:36:58 +0000

    chore(remix-dev-tools): add remix dev tools
    - update vite config to add rdt
    
    #3

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { vitePlugin as remix } from '@remix-run/dev'
+import { remixDevTools } from 'remix-development-tools'
 import { defineConfig } from 'vite'
 import { envOnlyMacros } from 'vite-env-only'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import rdtConfig from './rdt.config'
 import remixConfig from './remix.config'
 
 export default defineConfig({
-	plugins: [remix(remixConfig), tsconfigPaths(), envOnlyMacros()],
+	plugins: [
+		remixDevTools(rdtConfig),
+		remix(remixConfig),
+		tsconfigPaths(),
+		envOnlyMacros(),
+	],
 	server: {
 		host: process.env.NODE_ENV === 'production' ? undefined : true, // Allow access on local network
 	},
