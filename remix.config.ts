fac5a2395eb007a5545b3883fd792ac4097e2946
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-10-07 12:29:23 +0000

    chore(remix-flat-routes): add remix flat routes
    - add remix flat routes package
    - update remix config to use remix flat routes
    
    #3

--- remix.config.ts
+++ remix.config.ts
@@ -1,9 +1,12 @@
-import type { VitePluginConfig } from "@remix-run/dev";
+import type { VitePluginConfig } from '@remix-run/dev'
+import { flatRoutes } from 'remix-flat-routes'
 
 export default {
-    future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
+	future: {
+		v3_fetcherPersist: true,
+		v3_relativeSplatPath: true,
+		v3_throwAbortReason: true,
+	},
+	ignoredRouteFiles: ['**/*'],
+	routes: async defineRoutes => flatRoutes('routes', defineRoutes),
 } satisfies VitePluginConfig
