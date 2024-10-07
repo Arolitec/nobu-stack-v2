97328343e5d1818da79ec89844d0744299e30c00
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-10-07 15:13:52 +0000

    Setup msw (#21)
    * chore(deps): add msw package
    
    #20
    
    * chore(msw): setup mock service worker
    
    - add msw package
    - update server.js to also launch msw on dev env
    
    #20

---
+++ mocks/handlers.js
@@ -0,0 +1,9 @@
+// import { HttpResponse, http } from 'msw'
+
+export const handlers = [
+	// http.get('<MOCK_URL>', () => {
+	// 	return HttpResponse.json({
+	// 		message: 'Queued. Thank you.',
+	// 	})
+	// }),
+]
